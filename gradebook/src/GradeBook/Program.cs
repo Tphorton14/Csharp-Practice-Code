ee80fc2446fd6bd28365659482a3f23d704bb7e0
Tphorton <Tphorton14@gmail.com>
2020-04-03 08:36:39 -0400

    practicing debugging and creating first if/else statement in C sharp

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -8,8 +8,12 @@ namespace GradeBook
         // when dotnet runs it looks for "Main"
         static void Main(string[] args)
         {
+            if (args.Length > 0) {
             // whats executed when method runs
             Console.WriteLine($"Hello, {args[0]}!");
+            } else {
+                Console.WriteLine("HELLO!");
+            }
         }
     }
 }
